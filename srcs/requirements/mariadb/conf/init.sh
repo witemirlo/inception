62bfed8b8d84f1456fcdd2d17efd8c96ac77ab2d
witemirlo <jacoboberdugo@gmail.com>
2024-10-24 16:56:40 +0200

    [ADD] mariadb now does not exit

--- srcs/requirements/mariadb/conf/init.sh
+++ srcs/requirements/mariadb/conf/init.sh
@@ -1,3 +1,14 @@
 #! /bin/sh
 
-mariadb
+# mysql_install_db --user=mysql --datadir=/var/lib/mysql
+
+
+# ./etc/init.d/mariadb start
+
+# cat /etc/my.cnf.d/mariadb-server.cnf
+
+rc-service mariadb status
+
+mysqld --user=mysql
+
+mysqladmin -u root password ${MYSQL_ROOT_PASSWORD}
