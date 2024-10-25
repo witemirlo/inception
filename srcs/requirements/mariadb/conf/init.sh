3505db11baed4ab4b30072d1b33735ed8c2fffe4
witemirlo <jacoboberdugo@gmail.com>
2024-10-25 16:42:23 +0200

    comment

--- srcs/requirements/mariadb/conf/init.sh
+++ srcs/requirements/mariadb/conf/init.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
 
-chown -R mysql:mysql /var/lib/mysql/
+# chown -R mysql:mysql /var/lib/mysql/
 
-touch ddl_recobery.log
-chown mysql:mysql ddl_recobery.log
+# touch ddl_recobery.log
+# chown mysql:mysql ddl_recobery.log
 
-/usr/bin/mysql_install_db --user=mysql --datadir=/var/lib/mysql --force
+# /usr/bin/mysql_install_db --user=mysql --datadir=/var/lib/mysql --force
 
-mariadbd --user=mysql
+# mariadbd --user=mysql
