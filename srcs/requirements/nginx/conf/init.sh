b377cea8e91d6dbafa0a7ef5e589bb8694879fe5
witemirlo <jacoboberdugo@gmail.com>
2024-10-29 14:35:52 +0100

    working in link containers

--- srcs/requirements/nginx/conf/init.sh
+++ srcs/requirements/nginx/conf/init.sh
@@ -5,4 +5,7 @@ envsubst < /etc/nginx/http.d/server.conf.bak > /etc/nginx/http.d/server.conf
 
 openssl req -new -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/${NGINX_CERT} -keyout /etc/nginx/${NGINX_CERT_KEY} -subj "/C=ES/ST=Madrid/L=Madrid/O=42/OU=42/CN=jberdugo.42.fr"
 
+# usermod -aG www-data $LOGIN
+chown -R www:www /var/www/html/wordpress
+
 nginx -g "daemon off;"
