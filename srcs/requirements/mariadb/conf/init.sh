19ca27af1b1bcfc6410e07b5e567b764bc0b8ddf
witemirlo <jacoboberdugo@gmail.com>
2024-10-30 15:13:24 +0100

    working in wordpress container

--- srcs/requirements/mariadb/conf/init.sh
+++ srcs/requirements/mariadb/conf/init.sh
@@ -4,7 +4,7 @@
 
 chown -R mysql:mysql /var/lib/mysql/aria_log_control
 
-mv /tmp/setup-database /tmp/setup-database.bak
-envsubst < /tmp/setup-database.bak > /tmp/setup-database
+mv /tmp/setup-database.sql /tmp/setup-database.sql.bak
+envsubst < /tmp/setup-database.sql.bak > /tmp/setup-database.sql
 
-mariadbd --user=mysql --init-file="/tmp/setup-database"
+mariadbd --user=mysql --init-file="/tmp/setup-database.sql"
