52a8828ce16baeece50274327e90207b654b97f0
witemirlo <jacoboberdugo@gmail.com>
2024-11-01 19:34:59 +0100

    wip

--- srcs/requirements/mariadb/conf/init.sh
+++ srcs/requirements/mariadb/conf/init.sh
@@ -6,5 +6,6 @@ chown -R mysql:mysql /var/lib/mysql/aria_log_control
 
 mv /tmp/setup-database.sql /tmp/setup-database.sql.bak
 envsubst < /tmp/setup-database.sql.bak > /tmp/setup-database.sql
+chmod +x /tmp/setup-database.sql
 
 mariadbd --user=mysql --init-file="/tmp/setup-database.sql"
