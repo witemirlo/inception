b23cdaacbceb305f2dc178de2d1a0a58204f99de
witemirlo <jacoboberdugo@gmail.com>
2024-11-04 17:54:40 +0100

    installing wp

--- srcs/requirements/wordpress/conf/init.sh
+++ srcs/requirements/wordpress/conf/init.sh
@@ -2,4 +2,6 @@
 
 envsubst '${DATABASE_NAME} ${MARIADB_USER} ${MARIADB_USER_PASSWORD}' < /tmp/wp-config.php > /usr/share/webapps/wordpress/wp-config.php
 
+wp core install --path="/usr/share/webapps/wordpress" --url="jberdugo.42.fr" --title="Get Inceptioned" --locale="ES" --admin_user="${MARIADB_USER}" --admin_password="${MARIADB_USER_PASSWORD}" --admin_email="jbedugo@student.42madrid.fr" --skip-email
+
 php-fpm82 -F
