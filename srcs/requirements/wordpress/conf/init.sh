e4634844d215223075c827d84cce75db7dcfe6bb
witemirlo <jacoboberdugo@gmail.com>
2024-11-05 14:00:02 +0100

    clean script for setup wordpress

--- srcs/requirements/wordpress/conf/init.sh
+++ srcs/requirements/wordpress/conf/init.sh
@@ -2,6 +2,14 @@
 
 envsubst '${DATABASE_NAME} ${MARIADB_USER} ${MARIADB_USER_PASSWORD}' < /tmp/wp-config.php > /usr/share/webapps/wordpress/wp-config.php
 
-wp core install --path="/usr/share/webapps/wordpress" --url="${DOMAIN_NAME}" --title="Get Inceptioned" --locale="ES" --admin_user="${MARIADB_USER}" --admin_password="${MARIADB_USER_PASSWORD}" --admin_email="jbedugo@student.42madrid.fr" --skip-email
+wp core install \
+--path="/usr/share/webapps/wordpress" \
+--url="${DOMAIN_NAME}" \
+--title="Get Inceptioned" \
+--locale="ES" \
+--admin_user="${MARIADB_USER}" \
+--admin_password="${MARIADB_USER_PASSWORD}" \
+--admin_email="jbedugo@student.42madrid.fr" \
+--skip-email
 
 php-fpm82 -F
