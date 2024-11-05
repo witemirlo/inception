44e764ebb2133d93787761ea4f3298540ba9823d
witemirlo <jacoboberdugo@gmail.com>
2024-11-05 14:49:37 +0100

    cleaning things

--- srcs/requirements/mariadb/conf/init.sh
+++ srcs/requirements/mariadb/conf/init.sh
@@ -1,12 +1,16 @@
 #! /bin/sh
 
-# ./usr/bin/mariadb-install-db --verbose --user=mysql --datadir=/var/lib/mysql --force
+# change permissions in the working directory of mariadb
+chown -R mysql:mysql /var/lib/mysql
 
-chown -R mysql:mysql /var/lib/mysql/aria_log_control
-
+# create backup file
 mv /tmp/setup-database.sql /tmp/setup-database.sql.bak
 
+# expand enviroment variables and save it in the correct file
 envsubst '${DATABASE_NAME} ${MARIADB_USER} ${MARIADB_USER_PASSWORD} ${MARIADB_ROOT_PASSWORD}' < /tmp/setup-database.sql.bak > /tmp/setup-database.sql
+
+# set execute permissions
 chmod +x /tmp/setup-database.sql
 
+# run daemon
 mariadbd --user=mysql --init-file="/tmp/setup-database.sql"
